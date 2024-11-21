809cbe8564914580109f044a9436d81a58f88862
JustListening1 <nkarlace@gmail.com>
2024-11-21 15:16:08 +0800

    Add files via upload

---
+++ pawnshop/pawnshop/Buyingform.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace pawnshop
+{
+    public partial class Buyingform : Form
+    {
+        public Buyingform()
+        {
+            InitializeComponent();
+            
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void Buyingform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chb_iphone_CheckedChanged(object sender, EventArgs e)
+        {
+
+
+            if (chb_iphone.Checked)
+            {
+               
+                listBox1.Items.Add("iphone X ₱7,500");
+            }
+            else
+            {
+                
+                listBox1.Items.Remove("iphone X ₱7,500");
+            }
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox8.Checked)
+            {
+
+                listBox1.Items.Add("gold bar 5g 18karat");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("gold bar 5g 18karat");
+            }
+        }
+
+
+
+        private void chb_S23_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chb_S23.Checked)
+            {
+
+                listBox1.Items.Add("samsung s23");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("samsung s23");
+            }
+        }
+
+        private void chb_rog_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chb_rog.Checked)
+            {
+
+                listBox1.Items.Add("rog");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("rog");
+            }
+        }
+
+        private void chb_red_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chb_red.Checked)
+            {
+
+                listBox1.Items.Add("red");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("red");
+            }
+        }
+
+        private void chb_redmi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chb_redmi.Checked)
+            {
+
+                listBox1.Items.Add("Redmi");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("Redmi");
+            }
+        }
+
+        private void chb_A53_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chb_A53.Checked)
+            {
+
+                listBox1.Items.Add("samsung A53");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("samsung A53");
+            }
+        }
+
+        private void chb_shark_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (chb_shark.Checked)
+            {
+
+                listBox1.Items.Add("shark");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("shark");
+            }
+        }
+
+        private void checkBox9_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox9.Checked)
+            {
+
+                listBox1.Items.Add("Bracelet Rose Gold");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("Bracelet Rose Gold");
+            }
+
+        }
+
+        private void checkBox10_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox10.Checked)
+            {
+
+                listBox1.Items.Add("Ring Bi-Color");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("Ring Bi-Color");
+            }
+        }
+
+        private void checkBox11_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox11.Checked)
+            {
+
+                listBox1.Items.Add("Earring Tri-Color");
+            }
+            else
+            {
+
+                listBox1.Items.Remove("Earring Tri-Color");
+            }
+        }
+
+        private void checkBox12_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox12.Checked)
+            {
+
+                listBox1.Items.Add("Necklace White Gold");
+                
+            }
+            else
+            {
+
+                listBox1.Items.Remove("Necklace White Gold");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ pawnshop/pawnshop/Form2.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace pawnshop
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 482);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1335, 304);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Lucida Console", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(258, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(298, 129);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Buy";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Font = new System.Drawing.Font("Lucida Console", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(818, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(298, 129);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sell/Loan";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Gothic", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(349, 418);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(656, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "The PAWNSHOP where you can Buy, Sell, And Loan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(402, 370);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(526, 30);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "The Golden Key And Exchange";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(565, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Welcome To";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(477, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(354, 318);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Gothic", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(521, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(320, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "What are you here for?";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Lucida Console", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(12, 263);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(220, 38);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "back";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1335, 786);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
